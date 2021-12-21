1c9d6e7b935a466cedd432ce37f272c33ae1ddab
Anton Bugryakov <antis_amdg@mail.ru>
2021-12-21 22:19:41 +0300

    commit-1-hw-done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/cp4d3wqfqsvr3dfp/branch/main?svg=true)](https://ci.appveyor.com/project/Antis85/ra-hw8-hooks-context-use-json-fetch/branch/main)
+
 # ra-hw8-hooks-context-use-json-fetch
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Loading from './components/Loading';
+import Data from './components/Data';
+import Error from './components/Error';
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      <Loading url={process.env.REACT_APP_URL_LOADING_BUILD} />
+      <Data url={process.env.REACT_APP_URL_DATA_BUILD} />
+      <Error url={process.env.REACT_APP_URL_ERROR_BUILD} />
+    </>
+  );
+}
